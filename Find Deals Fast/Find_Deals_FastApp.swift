093f6475890493ed1be7e975538924c6cc17a907
ludik <bakhanetssimon@gmail.com>
2026-01-04 16:19:46 +0700

    Initial Commit

---
+++ Find Deals Fast/Find_Deals_FastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_Deals_FastApp.swift
+//  Find Deals Fast
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Find_Deals_FastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Find Deals FastTests/Find_Deals_FastTests.swift
@@ -0,0 +1,16 @@
+//
+//  Find_Deals_FastTests.swift
+//  Find Deals FastTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct Find_Deals_FastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
